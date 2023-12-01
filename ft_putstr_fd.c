cad80e909f7219c85b638bab8b850ec6cc8a4250
kenth <kennynavarre97@gmail.com>
2023-12-01 15:26:35 +0100

    modif de putstr mauvais include je sais pas pourquoi

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./libft/libft.h"
+#include "libft.h"
 
 void	ft_putstr_fd(char *s, int fd)
 {
